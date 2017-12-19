e3d55e1a6da7c17074299d0a78b25c8e464af3a4
Wali Rahman <wali.rahman89@yahoo.com>
2017-12-19 12:16:58 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,11 +1,12 @@
 passengers = {
-  suite_a: "Amanda Presley", 
-  suite_b: "Seymour Hoffman", 
-  suite_c: "Alfred Tennyson", 
-  suite_d: "Charlie Chaplin", 
+  suite_a: "Amanda Presley",
+  suite_b: "Seymour Hoffman",
+  suite_c: "Alfred Tennyson",
+  suite_d: "Charlie Chaplin",
   suite_e: "Crumpet the Elf"
   }
 
+winner = ''
 def select_winner(passengers)
   passengers.each do |suite, name|
   if suite == :suite_a && name.start_with?("A")
